bd5dee0a5c1a255120e86f1bc54e656f155a100d
rusminto <rusminto900@gmail.com>
2026-02-03 18:17:31 +0700

    restructure the project

--- templates/group-template-2/layouts/card-with-content-2/scripts/onLoad.js
+++
@@ -1,9 +0,0 @@
-export default async function onLoad(self){
-	const text = self.querySelector('#label-title');
-
-	let counter = 0;
-
-	setInterval(() => {
-		text.innerText = `COUNTER ${counter++}`
-	}, 3000);
-}
--- working-output/src/layouts/FilledCard/btnClick.js
+++
@@ -1,9 +0,0 @@
-export default async function btnClick(self){
-	self.querySelector('#btn-ok-a').addEventListener('click', () => {
-		new Notify ({
-    		status: 'error',
-    		title: 'GAGAL',
-    		text: 'OK ?'
-    	})
-	})
-}
